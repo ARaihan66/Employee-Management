818830595f597bc127a5f047f6beb52502fb50c3
Abu Raihan <aburaihancse066@gmail.com>
2026-05-03 17:54:38 +0600

    Working with employee modal

--- frontend/src/components/EmployeeModal.tsx
+++ frontend/src/components/EmployeeModal.tsx
@@ -1,4 +1,67 @@
+import { useState } from "react";
+import { backendUrl } from "../App";
+import { queryClient } from "../utils/queryClient";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
 const EmployeeModal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [info, setInfo] = useState(
+    type === "add"
+      ? { name: "", email: "", age: "", role: "", salary: "" }
+      : data,
+  );
+
+  const handleChange = (e) => {
+    setInfo((prev) => ({
+      ...prev,
+      [e.target.name]: [e.target.value],
+    }));
+  };
+
+  const addMutation = useMutation({
+    mutationFn: async (payload) => {
+      const res = await fetch(backendUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      const result = await res;
+
+      if (!res.ok) {
+        return new Error(result.error);
+      }
+
+      return result;
+    },
+    onSuccess: () => {
+      toast.success("Employee added successfully");
+      setIsOpen(false);
+      queryClient.invalidateQueries({ queryKey: ["employee_details"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const updateMutation = useMutation({
+    mutationFn: async (id: number, payload) => {
+      const res = await fetch(`${backendUrl}/${id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        return new Error(res.error);
+      }
+
+      return result;
+    },
+  });
+
   return <div>EmployeeModal</div>;
 };
 
--- frontend/src/components/EmployeeTable.tsx
+++ frontend/src/components/EmployeeTable.tsx
@@ -1,47 +1,67 @@
 import { TiEdit } from "react-icons/ti";
 import { MdDelete } from "react-icons/md";
 
-const EmployeeTable = ({ data }) => {
+interface Employee {
+  id: number;
+  email: string;
+  name: string;
+  age: number;
+  role: string;
+  salary: number;
+}
+
+interface EmployeeProps {
+  data?: {
+    employee: Employee[];
+  };
+}
+
+const EmployeeTable = ({ data }: EmployeeProps) => {
   return (
-    <div className="mx-32">
-      <table className="w-full">
-        <thead>
-          <tr className="border py-4">
-            <th className="border px-2">ID</th>
-            <th className="border px-2">Email</th>
-            <th className="border px-2">Name</th>
-            <th className="border px-2">Age</th>
-            <th className="border px-2">Role</th>
-            <th className="border px-2">Salary</th>
-            <th className="border px-2">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data?.employee?.map((item) => {
-              return (
-                <tr key={item.id} className="border text-center">
-                  <td className="border px-2 py-4">{item.id}</td>
-                  <td className="border px-2">{item.email}</td>
-                  <td className="border px-2">{item.name}</td>
-                  <td className="border px-2">{item.age}</td>
-                  <td className="border px-2">{item.role}</td>
-                  <td className="border px-2">{item.salary}</td>
-                  <td className="border px-2">
-                    <div className="flex justify-center gap-5">
-                      <button>
-                        <TiEdit className="text-xl" />
-                      </button>
-                      <button>
-                        <MdDelete className="text-xl" />
-                      </button>
-                    </div>
-                  </td>
-                </tr>
-              );
-            })}
-        </tbody>
-      </table>
+    <div className="w-full overflow-x-auto">
+      <div className="min-w-225 bg-gray-50 rounded-xl p-6">
+        <table className="w-full">
+          <thead>
+            <tr className="text-left text-sm text-gray-600">
+              <th className="py-3 px-4">ID</th>
+              <th className="py-3 px-4">Email</th>
+              <th className="py-3 px-4">Name</th>
+              <th className="py-3 px-4">Age</th>
+              <th className="py-3 px-4">Role</th>
+              <th className="py-3 px-4">Salary</th>
+              <th className="py-3 px-4">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data &&
+              data?.employee?.map((item) => {
+                return (
+                  <tr
+                    key={item.id}
+                    className="text-sm text-gray-700 rounded-2xl transition hover:bg-gray-200 hover:rounded-2xl shadow-sm"
+                  >
+                    <td className="p-4">{item.id}</td>
+                    <td className="p-4">{item.email}</td>
+                    <td className="p-4">{item.name}</td>
+                    <td className="p-4">{item.age}</td>
+                    <td className="p-4">{item.role}</td>
+                    <td className="p-4">{item.salary}</td>
+                    <td className="p-4">
+                      <div className="flex justify-start gap-4">
+                        <button className="bg-gray-200 p-2 rounded-full text-lg transition hover:bg-white cursor-pointer">
+                          <TiEdit />
+                        </button>
+                        <button className="bg-gray-200 p-2 text-red-600 rounded-full text-lg transition hover:bg-white cursor-pointer">
+                          <MdDelete />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
